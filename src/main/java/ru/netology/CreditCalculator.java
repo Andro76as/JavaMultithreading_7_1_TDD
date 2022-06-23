56d2b4815f6d78574f2da650c77df48e8a798e6d
Andro76as <andro76as@gmail.com>
2022-06-23 20:47:54 +0300

    Commit CreditCalculator

--- src/main/java/ru/netology/CreditCalculator.java
+++ src/main/java/ru/netology/CreditCalculator.java
@@ -2,19 +2,33 @@ package ru.netology;
 
 public class CreditCalculator {
     public static void main(String[] args) {
+        int sum = 2000000;
+        double rate = 11.8;
+        int month = 60;
 
+        CreditCalculator calculator = new CreditCalculator();
+
+        System.out.println("Ежемесячный платеж " + calculator.sumPerMonth(sum, rate, month));
+        System.out.println("Общая сумма за весь срок " + calculator.fullSumReturn(sum, rate, month));
+        System.out.println("Переплата за весь срок " + calculator.overPaySum(sum, rate, month));
 
     }
 
-    public int sumPerMonth(int sum, double rate, int period) {
-        return sum;
+    public int sumPerMonth(int sum, double rate, int month) {
+        double monthRate = (rate / 12) / 100;
+        double annuityRate = monthRate*(Math.pow(1+monthRate, month))/((Math.pow(1+monthRate, month))-1);
+        return (int) (sum*annuityRate);
     }
 
-    public int fullSumReturn(int sum, double rate, int period) {
-        return sum;
+    public int fullSumReturn(int sum, double rate, int month) {
+        double monthRate = (rate / 12) / 100;
+        double annuityRate = monthRate*(Math.pow(1+monthRate, month))/((Math.pow(1+monthRate, month))-1);
+        return (int) (sum*annuityRate*month);
     }
 
-    public int overPaySum(int sum, double rate, int period) {
-        return sum;
+    public int overPaySum(int sum, double rate, int month) {
+        double monthRate = (rate / 12) / 100;
+        double annuityRate = monthRate*(Math.pow(1+monthRate, month))/((Math.pow(1+monthRate, month))-1);
+        return (int) ((sum*annuityRate)*month - sum);
     }
 }
--- src/test/java/TestCreditCalculator.java
+++ src/test/java/TestCreditCalculator.java
@@ -3,29 +3,29 @@ import org.junit.jupiter.api.Assertions;
 import ru.netology.CreditCalculator;
 
 public class TestCreditCalculator {
-    private final int sum = 1000000;
+    private final int sum = 2000000;
     private final double rate = 11.8;
     private final int month = 60;
     CreditCalculator calculator = new CreditCalculator();
 
     @Test
     void testSumPerMonth() {
-        int expected = 32000;
-        int actual = (int) calculator.sumPerMonth(sum, rate, month);
+        int expected = 44287;
+        int actual = calculator.sumPerMonth(sum, rate, month);
         Assertions.assertEquals(expected, actual);
 
     }
 
     @Test
     void testFullSumReturn() {
-        int expected = 2000000;
+        int expected = 2657221;
         int actual = calculator.fullSumReturn(sum, rate, month);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void testOverPaySum() {
-        int expected = 500000;
+        int expected = 657221;
         int actual = calculator.overPaySum(sum, rate, month);
         Assertions.assertEquals(expected, actual);
     }
